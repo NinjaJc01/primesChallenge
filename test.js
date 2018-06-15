e5c0779fc86b1ab2793274425d237f9abc435cfc
NinjaJc01 <james@cookiehome.co.uk>
2018-06-15 10:40:04 +0100

    Upload

---
+++ test.js
@@ -0,0 +1,20 @@
+"use strict";
+const limit = 10000;
+var numbers = new Array();
+for (let i = 1; i<limit;i++) {numbers.push(i);}
+//console.log(numbers);
+var x = 2;
+var start = new Date()
+while (x < (Math.floor(limit/2))) 
+{
+    var y = 2;
+    while ((x*y) <= limit) 
+    {
+        if (numbers.includes(x*y)) {numbers.splice(numbers.indexOf(x*y),1);}
+        y+=1
+        //console.log(numbers);
+    }
+    x += 1;
+}
+//console.log(numbers);
+console.log((new Date() - start));
