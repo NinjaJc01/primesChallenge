7db4415364a7da837e2f221c54dae3f08d7a019f
NinjaJc01 <james@cookiehome.co.uk>
2018-06-15 18:03:46 +0100

    Fixed the code

--- testWeb.js
+++ testWeb.js
@@ -1,24 +1,26 @@
-function primes() {    
+function primes() {
     "use strict";
-    document.getElementById("results").innerText("Calculating...");
+    document.getElementById("results").textContent = "Calculating...";
     const limit = 10000;
-    var numbers = new Array();
-    for (let i = 2; i<limit;i++) {numbers.push(i);}
-    //console.log(numbers);
+    var numbers = [];
+    for (let i = 2; i<limit; i++)
+    {
+        numbers.push(i);
+    }
     var x = 2;
     var start = new Date()
-    while (x < (Math.floor(limit/2))) 
+    while (x < (Math.floor(limit/2)))
     {
         var y = 2;
-        while ((x*y) <= limit) 
+        while ((x*y) <= limit)
         {
-            if (numbers.includes(x*y)) {numbers.splice(numbers.indexOf(x*y),1);}
+            if (numbers.includes(x*y))
+            {
+                numbers.splice(numbers.indexOf(x*y),1);
+            }
             y+=1
-            //console.log(numbers);
         }
         x += 1;
     }
-    //console.log(numbers);
-    //console.log((new Date() - start));
-    document.getElementById("results").innerText(numbers+"\n "+(new Date() - start)+"ms");
+    document.getElementById("results").textContent = (numbers+"\n "+(new Date() - start)+"ms");
 }
